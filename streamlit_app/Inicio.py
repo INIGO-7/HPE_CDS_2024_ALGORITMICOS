481228f3601aec74601da7e62312288e28864286
jorgealcorta <jorgealcorta@opendeusto.es>
2024-04-28 12:16:18 +0200

    Tuneo Inicio streamlit

--- streamlit_app/Inicio.py
+++ streamlit_app/Inicio.py
@@ -9,27 +9,27 @@ st.set_page_config(
 
 ac.render_sidebar()
 
-# algoritmicos = r"""
-# ‎    __    __    ___  _____  ____  ____  ____  __  __  ____  ___  _____  ___ 
-#    /__\  (  )  / __)(  _  )(  _ \(_  _)(_  _)(  \/  )(_  _)/ __)(  _  )/ __)
-#   /(__)\  )(__( (_-. )(_)(  )   / _)(_   )(   )    (  _)(_( (__  )(_)( \__ \
-#  (__)(__)(____)\___/(_____)(_)\_)(____) (__) (_/\/\_)(____)\___)(_____)(___/
-# """
-# st.text(algoritmicos)
+algoritmicos = r"""
+‎    __    __    ___  _____  ____  ____  ____  __  __  ____  ___  _____  ___ 
+   /__\  (  )  / __)(  _  )(  _ \(_  _)(_  _)(  \/  )(_  _)/ __)(  _  )/ __)
+  /(__)\  )(__( (_-. )(_)(  )   / _)(_   )(   )    (  _)(_( (__  )(_)( \__ \
+ (__)(__)(____)\___/(_____)(_)\_)(____) (__) (_/\/\_)(____)\___)(_____)(___/
+"""
+st.text(algoritmicos)
 
-st.title("¡Bienvenidos a HealthAI!")
+st.title("¡Bienvenidos a HealthyAI!")
 st.subheader("Sobre nosotros")
 
 st.write("""
 <div style="text-align: justify;">
-    Somos un equipo de estudiantes de la Universidad de Deusto compuesto por Sara Hernandez, Aitor Hernandez, Unai Igartua, Jorge Alcorta y Iñigo Fernández-Sopeña. Nos enorgullece presentar nuestro proyecto de HealthAI, diseñado con el objetivo de ofrecer asesoramiento y orientación sobre primeros auxilios en una variedad de situaciones, desde heridas menores hasta emergencias médicas graves.<br><br>
+    Somos un equipo de estudiantes de la Universidad de Deusto compuesto por Sara Hernández, Aitor Hernández, Unai Igartua, Jorge Alcorta e Iñigo Fernández-Sopeña. Nos enorgullece presentar nuestro proyecto HealthyAI, diseñado con el objetivo de ofrecer asesoramiento y orientación sanitaria en una variedad de situaciones, desde heridas menores hasta emergencias médicas graves.<br><br>
 </div>
     """, unsafe_allow_html=True)
 
 st.subheader("Nuestro objetivo")
 st.write("""
 <div style="text-align: justify;">
-    El objetivo de esta herramienta que pueda ofrecer asesoramiento y orientación sobre primeros auxilios de manera clara y comprensible para conocer la posible enfermedad o herida que se tenga en cualquier momento, para cualquier usuario, sin importar su nivel de conocimientos médicos. Nos basamos en las capacidades que nos ofrece la Inteligencia Artificial para lograr este objetivo.<br><br>
+    El objetivo de esta herramienta es ofrecer orientación sobre primeros auxilios y assoramiento general de manera clara y comprensible para conocer la posible condición que se padezca en cualquier momento, para cualquier usuario, sin importar su nivel de conocimientos médicos. Nos basamos en las capacidades que nos ofrece la Inteligencia Artificial para lograr este objetivo.<br><br>
 </div>
     """, unsafe_allow_html=True)
 
